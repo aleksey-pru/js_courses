4d5e3de7e9ede538d20ff312ef563df28c6e1544
Aleksey <o.pruzhanskiy@gmail.com>
2023-03-12 16:21:32 +0200

    commit js_function_2_task

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,10 @@
-function printText () {
-
+function factorial(n) {
+    let fact = 1;
+    if (n==0) return fact;
+    for(let i=1; i<=n; i++) {
+        fact *= i;
+    }
+    return fact;
 }
+let a = factorial(5);
+console.log(a);
